42c928138ab8743013ebc4f6a2660c7f41b28f1e
Orjiugo Victor <orjiugo.victor@gmail.com>
2024-06-29 19:48:23 +0100

    Update Cards.jsx

--- OptimizeHK/src/components/Cards.jsx
+++ OptimizeHK/src/components/Cards.jsx
@@ -21,7 +21,7 @@ const Cards = () => {
 						<p className="py-2 border-b mx-8">Informed Decisions</p>
 					</div>
 					<button className="bg-[#f0ac44] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
-						Start Trial
+						Discover
 					</button>
 				</div>
 				<div className="w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
@@ -40,7 +40,7 @@ const Cards = () => {
 						<p className="py-2 border-b mx-8">Data Driven Growth</p>
 					</div>
 					<button className="bg-[#f0ac44] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
-						Start Trial
+						Thrive
 					</button>
 				</div>
 				<div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
@@ -49,15 +49,15 @@ const Cards = () => {
 						src={Triple}
 						alt="/"
 					/>
-					<h2 className="text-2xl font-bold text-center py-8">Single User</h2>
-					<p className="text-center text-4xl font-bold">$149</p>
+					<h2 className="text-2xl font-bold text-center py-8">For Economies</h2>
+					<p className="text-center text-4xl font-bold">B2G</p>
 					<div className="text-center font-medium">
-						<p className="py-2 border-b mx-8 mt-8">500 GB Storage</p>
-						<p className="py-2 border-b mx-8">1 Granted User</p>
-						<p className="py-2 border-b mx-8">Send up to 2 GB</p>
+						<p className="py-2 border-b mx-8 mt-8">Empowering SMEs</p>
+						<p className="py-2 border-b mx-8">Financial Inclusion</p>
+						<p className="py-2 border-b mx-8">Data Collection</p>
 					</div>
 					<button className="bg-[#f0ac44] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
-						Start Trial
+						Partner
 					</button>
 				</div>
 			</div>
